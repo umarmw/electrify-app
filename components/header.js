9e45284794cee7fe2f2cfedc3933a495cad6b3ca
Vashist Chamun <chandan.chamun@proximity.fr>
2021-06-21 21:39:14 +0400

    Initial commit

--- components/header.js
+++ components/header.js
@@ -1,12 +1,285 @@
-import Link from 'next/link'
+import Link from "next/link";
+import styled from "styled-components";
+import { useState } from "react";
+import { FaTimes, FaBars, FaSearch } from "react-icons/fa";
+
+const Navbar = styled.div`
+  background-color: #0b0f4d;
+  width: 100%;
+  position: fixed;
+  font-family: Omnes, Arial, sans-serif;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .container {
+    height: 60px;
+    padding: 0 20px 0 20px;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    //logo
+    .navbar-logo {
+      width: 150px;
+      height: auto;
+
+      .logo {
+        width: 100%;
+      }
+    }
+
+    .active {
+      right: 0;
+    }
+    .no-active {
+      right: -100%;
+    }
+
+    //navbar-links
+    .navbar-container {
+      position: fixed;
+      top: 18px;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-direction: row;
+      transition: all 0.3s;
+
+      &__clickable {
+        width: 20%;
+        transition: all 0.5s;
+        background-color: gray;
+        opacity: 0.2;
+      }
+
+      .navbar-content {
+        width: 80%;
+        background-color: #0b0f4d;
+        display: flex;
+        flex-direction: column;
+        padding: 24px 0 0 40px;
+
+        .navbar-left {
+          &__links {
+            display: flex;
+            flex-direction: column;
+            .left-link {
+            }
+          }
+        }
+        .navbar-right {
+          &__links {
+            display: flex;
+            flex-direction: column;
+            .right-link {
+              font-weight: 800;
+            }
+          }
+        }
+
+        .link {
+          color: #fff;
+          text-decoration: none;
+          line-height: 50px;
+          text-transform: uppercase;
+        }
+      }
+    }
+
+    .navbar-gadgets {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 3px;
+
+      &__search {
+        display: flex;
+        flex-direction: row-reverse;
+        align-items: center;
+
+        .searchbox {
+          background-color: #0b0f4d;
+          border: none;
+          border-bottom: 1px solid #fff;
+          color: #fff;
+
+          ::placeholder {
+            text-align: center;
+          }
+        }
+      }
+    }
+
+    .displaynone {
+      display: none;
+    }
+    .displayblock {
+      display: block;
+    }
+
+    .icons {
+      color: #fff;
+      padding: 5px;
+    }
+
+    .navbar-search {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 10px;
+    }
+  }
+
+  @media (min-width: 1033px) {
+    .container {
+      justify-content: center;
+      align-items: center !important;
+      margin: 0 2%;
+      padding: 0;
+      .navbar-container {
+        position: inherit;
+
+        &__clickable {
+          display: none;
+        }
+
+        .navbar-content {
+          width: 100%;
+          position: inherit;
+          flex-direction: row;
+          padding: 0;
+          justify-content: space-between;
+
+          .link {
+            padding: 10px;
+            line-height: normal;
+            display: flex;
+            align-items: center;
+          }
+
+          .navbar-left {
+            display: flex;
+            justify-content: flex-start;
+            padding-left: 30px;
+            &__links {
+              flex-direction: row;
+              font-size: 14px;
+
+              .left-link {
+                :first-child {
+                  display: none;
+                }
+              }
+            }
+          }
+          .navbar-right {
+            display: flex;
+            justify-content: flex-end;
+            width: 50px;
+            margin-top: -10px;
+            height: 80px;
+
+            &__links {
+              flex-direction: row;
+              font-size: 14px;
+              width: auto;
+
+              .right-link {
+                border-left: 1px solid gray;
+                border-right: 1px solid gray;
+              }
+            }
+          }
+        }
+      }
+      .mobile-nav {
+        display: none;
+      }
+    }
+  }
+`;
 
 export default function Header() {
+  const [isClicked, setClicked] = useState();
+
+  const toggleClicked = () => {
+    setClicked(!isClicked);
+  };
+
+  const [isClickedSearchIcon, setClickedSearchIcon] = useState();
+
+  const toggleSearch = () => {
+    setClickedSearchIcon(!isClickedSearchIcon);
+  };
+
   return (
-    <h2 className="">
-      <Link href="/">
-        <a className="">Blog</a>
-      </Link>
-      .
-    </h2>
-  )
+    <Navbar>
+      <div className="container">
+        <div className="navbar-logo">
+          <Link href="/">
+            <img className="logo" src="images/logo.svg" />
+          </Link>
+        </div>
+
+        <div
+          className={`navbar-container ${isClicked ? "active" : "no-active"}`}
+        >
+          <div
+            className="navbar-container__clickable"
+            onClick={toggleClicked}
+          ></div>
+          <div className="navbar-content">
+            <div className="navbar-left">
+              <div className="navbar-left__links">
+                <Link href="/">
+                  <a className="left-link link">Home</a>
+                </Link>
+                <Link href="/">
+                  <a className="left-link link">Locate a charger</a>
+                </Link>
+                <Link href="/">
+                  <a className="left-link link">How it works</a>
+                </Link>
+                <Link href="/">
+                  <a className="left-link link">Pricing</a>
+                </Link>
+                <Link href="/">
+                  <a className="left-link link">Mobile app</a>
+                </Link>
+              </div>
+            </div>
+
+            <div className="navbar-right">
+              <div className="navbar-right__links">
+                <Link href="/">
+                  <a className="right-link link">Home products</a>
+                </Link>
+                <Link href="/">
+                  <a className="right-link link">Commercial solutions</a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="navbar-gadgets">
+          <div className="navbar-gadgets__search">
+            <div className="navbar-search icons" onClick={toggleSearch}>
+              {isClickedSearchIcon ? <FaTimes /> : <FaSearch />}
+            </div>
+            <div className={isClickedSearchIcon ? "" : "displaynone"}>
+              <input type="text" placeholder="SEARCH" className="searchbox" />
+            </div>
+          </div>
+
+          <div className={isClickedSearchIcon ? "displaynone" : ""}>
+            <div className="mobile-nav icons" onClick={toggleClicked}>
+              {isClicked ? <FaTimes /> : <FaBars />}
+            </div>
+          </div>
+        </div>
+      </div>
+    </Navbar>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
-import Head from 'next/head'
-import { CMS_NAME } from '../lib/constants'
+import Container from "../components/container";
+import MoreStories from "../components/more-stories";
+import HeroPost from "../components/hero-post";
+import Intro from "../components/intro";
+import Header from "../components/header";
+import Layout from "../components/layout";
+import { getAllPostsForHome } from "../lib/api";
+import Head from "next/head";
+import { CMS_NAME } from "../lib/constants";
 
 export default function Index({ preview, allPosts }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+  const heroPost = allPosts[0];
+  const morePosts = allPosts.slice(1);
   return (
     <>
       <Layout preview={preview}>
@@ -17,7 +18,8 @@ export default function Index({ preview, allPosts }) {
           <title>Next.js Blog Example with {CMS_NAME}</title>
         </Head>
         <Container>
-          <Intro />
+          <Header />
+          {/* <Intro />
           {heroPost && (
             <HeroPost
               title={heroPost.title}
@@ -28,16 +30,16 @@ export default function Index({ preview, allPosts }) {
               excerpt={heroPost.excerpt}
             />
           )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />} */}
         </Container>
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getStaticProps({ preview = false }) {
-  const allPosts = (await getAllPostsForHome(preview)) ?? []
+  const allPosts = (await getAllPostsForHome(preview)) ?? [];
   return {
     props: { preview, allPosts },
-  }
+  };
 }
