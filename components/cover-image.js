18885a43aa7eff09d766dd1dae7f92b2d3e72312
abubakar <abubakar.chonnoo@hangarww.com>
2021-06-23 15:21:33 +0400

    Added Hover on Hero Post

--- components/cover-image.js
+++ components/cover-image.js
@@ -1,10 +1,16 @@
 import Link from 'next/link'
+import styled from 'styled-components'
+
+const Img_Block = styled.img`
+  display: block;
+`;
+
 
 export default function CoverImage({ title, url, slug }) {
   const image = (
     <picture>
       <source className={[slug, "img--responsive img--rounded"].join(" ")} srcSet={`${url}?fm=webp&w=425`} type="image/webp" />
-      <img
+      <Img_Block
         src={`${url}?w=425`}
         alt={`Cover Image for ${title}`}
         className={[slug, "img--responsive img--rounded"].join(" ")}
